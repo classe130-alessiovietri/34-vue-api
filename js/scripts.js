10ebdee8aa2e5743f9ca29e61a3c902e1ba9e891
Alessio Vietri <info@alessiovietri.it>
2024-09-02 13:19:53 +0200

    Completato LC

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,26 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-            message: 'Template HTML CSS Vue'
+            message: '34 Vue API',
+            email: ''
         };
+    },
+    created() {
+        console.log('PRIMA DELLE CHIAMATE API');
+        
+        for (let i = 0; i < 5; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp) => {
+                    // console.log('La Promise ha ottenuto un risultato');
+                    // console.log('Intero oggetto risposta:', resp);
+                    // console.log('Risposta dell\'API:', resp.data);
+                    console.log('Dato email della risposta API ' + i + ':', resp.data.response);
+
+                    this.email = resp.data.response;
+                });
+        }
+
+        console.log('DOPO LE CHIAMATE API');
     }
 }).mount('#app');
